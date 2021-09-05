cca5782a1199242af10dbc103ed2c41cc276a6e8
Aleksander Dyatlov <woodpecker.aleks@gmail.com>
2021-09-05 13:19:03 +0300

    Added port from env

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const app = require('express')()
 const { server } = require('./config.json')
+const port = process.env.PORT ?? server.port
 const routes = require('./routes/index')
 
 for (const route of Object.values(routes)) {
   app.use('/api', route)
 }
 
-app.listen(server.port, () => console.log(`Server running on port ${server.port}`))
+app.listen(port, () => console.log(`Server running on port ${port}`))
